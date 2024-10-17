f57e35df33ad3ad973aabd7d65bd5e10acc71b79
Jaume Fernández <jaumefernandez@carnicassunyer.com>
2024-10-17 11:48:29 +0200

    incluida categoria de populares en el home y arreglado el problema del slider al ser de movil

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,93 +1,15 @@
-import React, { useEffect, useState, useRef } from 'react';
-import api from '../utils/api';
-import ProductCard from '../components/ProductCard';
+import React from 'react';
 import HeroSection from '../components/HeroSection';
+import NewSeasonSection from '../components/NewSeasonSection';
+import PopularProductsSection from '../components/PopularProductsSection';
 import './css/HomePage.css';
-import { Link } from 'react-router-dom';
-import Slider from 'react-slick';
-import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const HomePage = () => {
-    const [products, setProducts] = useState([]);
-    const sliderRef = useRef(null);
-
-    useEffect(() => {
-        const fetchNewProducts = async () => {
-            try {
-                const res = await api.get('/products', {
-                    params: { category: 'NUEVOS' }
-                });
-                setProducts(res.data);
-            } catch (error) {
-                console.error('Error al obtener productos de la categoría "NUEVOS":', error);
-            }
-        };
-
-        fetchNewProducts();
-    }, []);
-
-    const settings = {
-        dots: false,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                }
-            }
-        ]
-    };
-
-    const handlePrev = () => {
-        sliderRef.current.slickPrev();
-    };
-
-    const handleNext = () => {
-        sliderRef.current.slickNext();
-    };
-
     return (
         <div className="homepage">
             <HeroSection />
-            <div className="homepage__header">
-                <div className='title-header'>
-                    <h2 className="featured-title">NEW SEASON + NEW DROPS = MORE PROGRESS</h2>
-                    <Link to="/category/nuevos" className="view-all">View All</Link>
-                </div>
-                <div className="carousel__controls">
-                    <button onClick={handlePrev} className="carousel__btn">
-                        <FaChevronLeft />
-                    </button>
-                    <button onClick={handleNext} className="carousel__btn">
-                        <FaChevronRight />
-                    </button>
-                </div>
-            </div>
-            <Slider {...settings} ref={sliderRef} className="homepage__products">
-                {products.map((product) => (
-                    <div key={product._id} className="product-slide">
-                        <ProductCard product={product} />
-                    </div>
-                ))}
-            </Slider>
+            <NewSeasonSection />
+            <PopularProductsSection />
         </div>
     );
 };
